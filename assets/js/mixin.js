3b61b7bb5d2d01959195757d00455a994de8f2b8
Philippe Lafrance <phil.laf@gmail.com>
2015-09-11 04:57:21 -0400

    First draft of the websocket service and bindings with react

---
+++ assets/js/mixin.js
@@ -0,0 +1,64 @@
+define(function(require) {
+
+    var websocket = require('gintonic_c_m_s/js/websocket/websocket');
+    var $ = require('jquery');
+
+    var CommunicationMixin = {
+
+        componentDidMount: function() {
+            websocket.subscribe({
+                topic: this.recieveUri,
+                method: this.recieve,
+            });
+            this.fetch();
+        },
+
+        recieve: function(data){
+            var data = JSON.parse(data[0]);
+            this.recieved(data);
+        },
+
+        fetch: function(){
+            var that = this;
+            $.ajax({
+                url: this.fetchUrl,
+                method: "POST",
+                dataType: 'json',
+                cache: false,
+                data: {
+                    id: [this.props.id]
+                }
+            })
+            .done(function(data){
+                that.fetched(data);
+            })
+            .fail(function(data){
+                console.log('fail');
+                console.log(data);
+            });
+        }, 
+
+        send: function(data) {
+            var that = this;
+            $.ajax({
+                url: this.sendUrl,
+                method: "POST",
+                dataType: 'json',
+                cache: false,
+                data: data
+            })
+            .done(function(data){
+                if ( $.isFunction(that.sent) ) {
+                    that.sent(data);
+                }
+            })
+            .fail(function(data){
+                console.log('fail');
+                console.log(data);
+            });
+        },
+    };
+    
+    return CommunicationMixin;
+
+});
---
+++ assets/js/websocket.js
@@ -0,0 +1,75 @@
+define(function(require) {
+
+    var autobahn = require('autobahn');
+    var instance = null;
+
+    var user = "1";
+    var key = "phil.laf@gmail.com";
+
+    function Websocket() {
+
+        if (instance != null) {
+            return;
+        }
+
+        this.subscribes = [];
+        this.session = null;
+        that = this;
+
+        var connection = new autobahn.Connection({
+            url: 'ws://127.0.0.1:9090/ws',
+            realm: 'realm1',
+            authmethods: ["wampcra"],
+            authid: user,
+            onchallenge: onchallenge
+        });
+
+        connection.onopen = function (session, details) {
+            console.log("connected session with ID " + session.id);
+            console.log("authenticated using method '" + details.authmethod + "' and provider '" + details.authprovider + "'");
+            console.log("authenticated with authid '" + details.authid + "' and authrole '" + details.authrole + "'");
+            that.session = session;
+            that.subscribes.forEach(function(sub) {
+                session.subscribe(sub['topic'], sub['method'])
+            });
+        };
+
+        connection.onclose = function () {
+            console.log("disconnected", arguments);
+        }
+
+        connection.open();
+    }
+
+    function onchallenge (session, method, extra) {
+        console.log(method, extra);
+        if (method === "wampcra") {
+            var keyToUse = key;
+            if (typeof extra.salt !== 'undefined') {
+                keyToUse = autobahn.auth_cra.derive_key(key, extra.salt);
+            }
+            console.log("authenticating via '" + method + "' and challenge '" + extra.challenge + "'");
+            return autobahn.auth_cra.sign(keyToUse, extra.challenge);
+        } else {
+            throw "don't know how to authenticate using '" + method + "'";
+        }
+    }
+
+    Websocket.prototype.subscribe = function(sub) {
+        if (this.session == null) {
+            this.subscribes.push(sub);
+        } else {
+            this.session.subscribe(sub['topic'], sub['method']);
+        }
+    }
+
+    Websocket.getInstance = function(){
+        if (instance === null) {
+            instance = new Websocket();
+        }
+        return instance;
+    }
+
+    return Websocket.getInstance();
+
+});
